463f67b7d2a2cd5ce1be009bf883a289131fdc2e
jhun0601 <ngjhunnen@gmail.com>
2021-11-23 09:05:19 +0800

    change url search

--- public/js/app.js
+++ public/js/app.js
@@ -3,15 +3,10 @@ const search = document.getElementById("searchLocation");
 const messageOne = document.getElementById("message");
 const result = document.getElementById("result");
 
-const port = process.env.PORT;
-
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
   messageOne.textContent = "Loading . . . ";
   result.textContent = "";
-  if (port === 5000) {
-    const url = "http://localhost:5000";
-  }
   fetch("/weather?address=" + search.value).then((response) => {
     console.log(port);
     console.log(url);
