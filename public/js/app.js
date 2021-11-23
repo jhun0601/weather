3e2fbc5292bec84389adaa4a1070cc05af324b9a
jhun0601 <ngjhunnen@gmail.com>
2021-11-23 09:01:56 +0800

    change url search

--- public/js/app.js
+++ public/js/app.js
@@ -2,20 +2,26 @@ const weatherForm = document.querySelector("form");
 const search = document.getElementById("searchLocation");
 const messageOne = document.getElementById("message");
 const result = document.getElementById("result");
+
+const port = process.env.PORT;
+
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
   messageOne.textContent = "Loading . . . ";
   result.textContent = "";
-  fetch("http://localhost:5000/weather?address=" + search.value).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-        } else {
-          messageOne.textContent = data.location;
-          result.textContent = data.forecast;
-        }
-      });
-    }
-  );
+  if (port === 5000) {
+    const url = "http://localhost:5000";
+  }
+  fetch("/weather?address=" + search.value).then((response) => {
+    console.log(port);
+    console.log(url);
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.location;
+        result.textContent = data.forecast;
+      }
+    });
+  });
 });
